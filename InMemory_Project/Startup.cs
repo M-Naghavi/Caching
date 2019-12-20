ad5100f6858aa368addf78d78f10b1499b345aa1
Ali Naghavi <mad.Ali.Naghavi@gmail.com>
2019-12-20 14:53:43 +0330

    ok

--- InMemory_Project/Startup.cs
+++ InMemory_Project/Startup.cs
@@ -15,48 +15,31 @@ namespace InMemory_Project
 {
     public class Startup
     {
+        string contentRoot = string.Empty;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            contentRoot = configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ProjectContext>(
-               (options) =>
-               {
-                   options.UseSqlite("Data Source=app.sqlite");
-               }
-           );
-            services.AddMemoryCache();
+            services.AddDbContext<ProjectContext>(options => options.UseSqlite($"Data Source={contentRoot}\\app.sqlite"));
+            //services.AddDbContext<ProjectContext>(options => options.UseSqlite(@"Data Source=D:\Work\GitHub\Caching\InMemory_Project\app.sqlite"));
 
+            services.AddMemoryCache();
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
