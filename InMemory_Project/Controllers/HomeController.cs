b1a1aa8cbf19e7de51fbffca7248a97243583e03
Ali Naghavi <mad.Ali.Naghavi@gmail.com>
2019-12-20 10:54:36 +0330

    Database Created

--- InMemory_Project/Controllers/HomeController.cs
+++ InMemory_Project/Controllers/HomeController.cs
@@ -5,25 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using InMemory_Project.Models;
+using InMemory_Project.Data;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace InMemory_Project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ProjectContext _db;
+        private IMemoryCache _cache;
+        private static ILogger<HomeController> _logger;
+
+        public HomeController(ProjectContext db, IMemoryCache cache, ILogger<HomeController> logger)
+        {
+            _db = db;
+            _cache = cache;
+            _logger = logger;
+        }
+
+        // in memort cahche
         public IActionResult Index()
         {
-            return View();
+            IList<User> model;
+            if (!_cache.TryGetValue("users", out model))
+            {
+                model = _db.Users.ToList();
+
+                var cacheEntryOption = new MemoryCacheEntryOptions()
+                   .SetPriority(CacheItemPriority.NeverRemove)
+                   //.SetAbsoluteExpiration(TimeSpan.FromDays(1))
+                   .SetSlidingExpiration(TimeSpan.FromSeconds(3))
+                   .RegisterPostEvictionCallback(UserCahceEvicated);
+                _cache.Set("users", model, cacheEntryOption);
+            }
+            return View(model);
+        }
+        private static void UserCahceEvicated(object key, object value, EvictionReason reason, object state)
+        {
+            _logger.LogWarning($"users cache evicated : {reason} | state: {state}");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ InMemory_Project/Data/ProjectContext.cs
@@ -0,0 +1,20 @@
+﻿using InMemory_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemory_Project.Data
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext
+            (DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
